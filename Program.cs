dbd7208add18d16f57cc13f9b8fcebc1dff4afec
Thenewbayan <chemspirit.trade@gmail.com>
2023-03-20 20:53:41 +0300

    prigram OK

--- Program.cs
+++ Program.cs
@@ -2,9 +2,17 @@
 // и формирует массив в котором количество символов меньше или равно 3
 // задать с клавиатуры или на старте программы, не использовать коллекции
 
-string [] words= {"rex", "text", "sdfsdf"};
+Console.WriteLine("Давайте выведем на экран только те элементы нашего массива, размер которых 3 или меньше символов");
+Console.WriteLine("Укажите количество слов, которое вы хотите проверить");
+int quantity=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Необходимые слова водите через  ENTER");
+string [] words= new string [quantity];//создали массив из строчных элементов и его заполнили
+for (int k=0; k<quantity; k++)
+{
+    words[k]=Console.ReadLine();
+}
 string [] normalWords= new string [words.Length];
-for (int i=0; i<words.Length; i++)
+for (int i=0; i<words.Length; i++)//проверка на количество символов в каждом элементе
 {
     if (words[i].Length<4)
     {
@@ -14,5 +22,5 @@ for (int i=0; i<words.Length; i++)
         }
     }
 }
-Console.WriteLine(String.Join(", ", words));
-Console.WriteLine(String.Join(", ", normalWords));
+Console.WriteLine(String.Join(", ", words));//вывод первого массива
+Console.WriteLine(String.Join(", ", normalWords));//вывод контрольного массива
